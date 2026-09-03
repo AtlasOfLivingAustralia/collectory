2a76566bfa7aba369787d99e45e30365f3a15ea8
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-09-03 10:41:39 +1000

    Fix for startup errors when biocache-ws is unavilable

--- grails-app/init/collectory/BootStrap.groovy
+++ grails-app/init/collectory/BootStrap.groovy
@@ -4,15 +4,19 @@ class BootStrap {
     def messageSource
     def application
     def grailsApplication
+    private static final String[] MESSAGE_BASENAMES = [
+            "file:///var/opt/atlas/i18n/collectory-plugin/messages",
+            "file:///opt/atlas/i18n/collectory-plugin/messages",
+            "WEB-INF/grails-app/i18n/messages",
+            "classpath:messages"
+    ] as String[]
 
     def init = { servletContext ->
-        messageSource.setBasenames(
-                "file:///var/opt/atlas/i18n/collectory-plugin/messages",
-                "file:///opt/atlas/i18n/collectory-plugin/messages",
-                "WEB-INF/grails-app/i18n/messages",
-                "classpath:messages",
-                "${application.config.biocacheServicesUrl}/facets/i18n"
-        )
+        List<String> basenames = MESSAGE_BASENAMES as List<String>
+        if (application.config.biocacheServicesUrl) {
+            basenames << "${application.config.biocacheServicesUrl}/facets/i18n"
+        }
+        messageSource.setBasenames(*basenames as String[])
 
         // gbifDefaultEntityCountry is mandatory. Not validating this value as 'ZZZ'
         if (!grailsApplication.config.gbifDefaultEntityCountry) {
