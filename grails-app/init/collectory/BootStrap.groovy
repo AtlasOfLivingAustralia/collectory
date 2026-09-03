b4d9e2d30183ba716f46aaadd31e2236af0e9b1a
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-09-03 10:49:29 +1000

    Second fix for startup errors

--- grails-app/init/collectory/BootStrap.groovy
+++ grails-app/init/collectory/BootStrap.groovy
@@ -1,5 +1,7 @@
 package collectory
 
+import java.net.HttpURLConnection
+
 class BootStrap {
     def messageSource
     def application
@@ -13,8 +15,21 @@ class BootStrap {
 
     def init = { servletContext ->
         List<String> basenames = MESSAGE_BASENAMES as List<String>
-        if (application.config.biocacheServicesUrl) {
-            basenames << "${application.config.biocacheServicesUrl}/facets/i18n"
+        def biocacheServicesUrl = application.config.biocacheServicesUrl
+        if (biocacheServicesUrl) {
+            String facetsI18nUrl = "${biocacheServicesUrl}/facets/i18n"
+            HttpURLConnection connection = null
+            try {
+                connection = (HttpURLConnection) new URL(facetsI18nUrl).openConnection()
+                connection.setRequestMethod("GET")
+                connection.setConnectTimeout(2000)
+                connection.setReadTimeout(2000)
+                if (connection.responseCode >= 200 && connection.responseCode < 300) {
+                    basenames << facetsI18nUrl
+                }
+            } finally {
+                connection?.disconnect()
+            }
         }
         messageSource.setBasenames(*basenames as String[])
 
